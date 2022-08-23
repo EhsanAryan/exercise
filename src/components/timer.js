914b47623adaa99d508f61ae32649ea26a03697d
Ehsan <ehsan.aryan2001@gmail.com>
2022-08-23 18:29:54 +0430

    The timer with start and stop and reset button

--- src/components/timer.js
+++ src/components/timer.js
@@ -10,45 +10,82 @@ export default class Timer extends React.Component {
     constructor(){
         super();
         this.state = {
-            time : 12
+            hour: 0 ,
+            minute: 0 ,
+            second: 0 ,
+            isStart: false
         }
     }
-
-
-    componentDidUpdate(){
-        if(this.state.time===0){
-            clearInterval(interval);
-        }
-    }
-
 
     startTimer = () => {
-        if(!interval){
-            interval = setInterval(()=>{
-                this.setState({
-                    time : this.state.time - 1
-                });
+        if(this.state.isStart===false){
+            this.setState({
+                isStart : true
+            });
+
+            interval = setInterval(() => {
+                if(this.state.second < 59){
+                    this.setState({
+                        second : this.state.second + 1
+                    });
+                }
+                else {
+                    if(this.state.minute < 59){
+                        this.setState({
+                            second : 0 ,
+                            minute : this.state.minute + 1
+                        });
+                    }
+                    else {
+                        this.setState({
+                            second : 0 ,
+                            minute : 0 ,
+                            hour : this.state.hour + 1
+                        });
+                    }
+                }
+
+
+
+
             } , 1000);
         }
     }
 
 
     stopTimer = () =>{
-        if(interval){
+        if(this.state.isStart===true){
+            this.setState({
+                isStart : false
+            });
             clearInterval(interval);
-            interval=undefined;
         }
     }
 
 
+    resetTimer = () => {
+        this.stopTimer();
+        this.setState({
+            hour : 0 ,
+            minute : 0 ,
+            second : 0
+        });
+        console.log(this.state.hour , this.state.minute , this.state.isStart);
+    }
+
+
     render(){
+        let h = this.state.hour;
+        let m = this.state.minute;
+        let s = this.state.second;
 
         return (
             <div className="timer-div">
-                Time: {this.state.time}
+                {`${h>9 ? h : "0"+h} : ${m>9 ? m : "0"+m} : ${s>9 ? s : "0"+s}`}
                 <span>
-                    <button onClick={this.startTimer}>Start</button>
-                    <button onClick={this.stopTimer}>Stop</button>
+                    <button onClick={this.startTimer} className="timer-btn">Start</button>
+                    <button onClick={this.stopTimer} className="timer-btn">Stop</button>
+                    <button onClick={this.resetTimer} className="timer-btn">Reset</button>
                 </span>
             </div>
         );
